714b9eaeb30f738eb42262136310cb1aac0a698d
Elayne M. Castillo <87791968+layniecast@users.noreply.github.com>
2026-07-18 14:21:30 -0400

    Update scriptButton.js

--- scriptButton.js
+++ scriptButton.js
@@ -1,14 +1,4 @@
 const likeBtn = document.getElementById("likeBtn");
 const likeCount = document.getElementById("likeCount");
 
-// Load initial likes from localStorage
-let likes = localStorage.getItem("totalLikes")
-  ? parseInt(localStorage.getItem("totalLikes"))
-  : 0;
-likeCount.textContent = likes;
-
-likeBtn.addEventListener("click", () => {
-  likes++;
-  localStorage.setItem("totalLikes", likes); // Save to local storage
-  likeCount.textContent = likes;
-});
+likeBtn.addEventListener('click', () => likeCount.innerHTML++);
