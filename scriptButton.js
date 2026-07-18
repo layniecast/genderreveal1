ba7126dea5e4d6b18d07c6d32a701f9dc02670a1
Elayne M. Castillo <87791968+layniecast@users.noreply.github.com>
2026-07-18 16:10:00 -0400

    Update scriptButton.js

--- scriptButton.js
+++ scriptButton.js
@@ -2,9 +2,7 @@ const likeBtn = document.getElementById("likeBtn");
 const likeCount = document.getElementById("likeCount");
 
 let count = 0;
-let isliked= false;
-
 likeBtn.addEventListener('click', () => {
-  if (!isLiked) {
-  count++}};
-likeCount.innerText = count;
+  count++;
+  likeCount.textCount = count;
+});
